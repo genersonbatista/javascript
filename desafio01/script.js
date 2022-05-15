677a005bf36ee1a294e7d1de5b960ea1c1c7f65d
Generson Batista <genersonbatista16@gmail.com>
2022-05-15 18:49:57 -0300

    finalizado desafio01 js

--- desafio01/script.js
+++ desafio01/script.js
@@ -4,15 +4,20 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    hora = 10
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 0 && hora < 12){
+        document.body.style.background = '#00BFFF'
+        msg.innerHTML = `Agora são ${hora} horas. Bom dia!`
         imagens.src = 'manha.png'
 
     } else if(hora >=12 && hora < 18){
+        document.body.style.background = '#ADFF2F'
         imagens.src = 'tarde.png'
+        msg.innerHTML = `Agora são ${hora} horas. Boa tarde!`
 
     } else{
+        document.body.style.background = '#000080'
         imagens.src = 'noite.png'
+        msg.innerHTML = `Agora são ${hora} horas. Boa Noite!`
     }
 }
